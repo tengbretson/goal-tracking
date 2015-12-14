7abcb0af20939e4a87d4ff1c88da7a15f5d7cc0f
Tanner Engbretson <tanner@engbretson.us>
2015-12-13 23:30:37 -0600

    Made adjustments to the plotting, added a readme.

--- src/controllers/my-history.js
+++ src/controllers/my-history.js
@@ -1,5 +1,5 @@
 import d3 from 'd3';
-import { pluck } from 'lodash';
+import { pluck, map } from 'lodash';
 
 export default function MyHistoryController($scope, goals, times) {
   $scope.goals = goals;
@@ -8,57 +8,51 @@ export default function MyHistoryController($scope, goals, times) {
 
   $scope.$watch('historyModel.goalShowing', () => {
     const goalId = $scope.historyModel.goalShowing;
-
-    // lets convert firebase object to array
-    let t = [];
-      console.log(times);
-
-
+    const yearShowing = '2015';
+    const data = map(times[yearShowing] || [], (obj, i) => {
+      return { day: i, times: obj[goalId] || null };
+    }).filter(({ times }) => !!times);
+    renderTimeseries(data, goals.$getRecord(goalId));
   });
 }
 
-/*
+function renderTimeseries(data, goal) {
+  const margin = { top: 12, right: 12, bottom: 20, left: 64 };
+  const width = 360 - margin.left - margin.right;
+  const height = 240 - margin.top - margin.bottom;
 
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+  const x = d3.scale.linear().range([0, width]);
+  const y = d3.scale.linear().range([height, 0]);
 
-var parseDate = d3.time.format('%d-%b-%y').parse;
+  const xAxis = d3.svg.axis().scale(x).orient('bottom');
+  const yAxis = d3.svg.axis().scale(y).orient('left');
 
-var x = d3.time.scale()
-    .range([0, width]);
+  const area = d3.svg.area().x(d => x(d.day)).y1(d => y(d.times)).y0(height);
+  const goalLine = d3.svg.line().x(d => x(d.day)).y(d => y(d.times));
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+  const goalLineData = [
+    { times: goal ? goal.times : 0, day: 0 },
+    { times: goal ? goal.times : 0, day: 364 }
+  ];
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient('bottom');
+  const svg = d3.select('.chart-zone').html('').append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient('left');
+  x.domain(d3.extent(data, d => d.day));
+  y.domain([0, d3.max([...data, ...goalLineData], d => d.times)]);
 
-var line = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
+  svg.append('path')
+      .datum(data)
+      .attr('class', 'area')
+      .attr('d', area);
 
-var svg = d3.select('.chart-zone').append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-d3.tsv('data.tsv', function(error, data) {
-  if (error) throw error;
-
-  data.forEach(function(d) {
-    d.date = parseDate(d.date);
-    d.close = +d.close;
-  });
-
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain(d3.extent(data, function(d) { return d.close; }));
+  svg.append('path')
+      .datum(goalLineData)
+      .attr('class', 'goal')
+      .attr('d', goalLine);
 
   svg.append('g')
       .attr('class', 'x axis')
@@ -70,15 +64,8 @@ d3.tsv('data.tsv', function(error, data) {
       .call(yAxis)
     .append('text')
       .attr('transform', 'rotate(-90)')
-      .attr('y', 6)
+      .attr('y', -48)
       .attr('dy', '.71em')
       .style('text-anchor', 'end')
-      .text('Price ($)');
-
-  svg.append('path')
-      .datum(data)
-      .attr('class', 'line')
-      .attr('d', line);
-});
-
-*/
+      .text('Times');
+}
